5deb879f4a7af891cf7e1e9fe42dcdbf9868132d
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-25 16:37:29 -0300

    hamburger menu

--- portfolio/src/Components/NavBar/Navbar.jsx
+++ portfolio/src/Components/NavBar/Navbar.jsx
@@ -3,55 +3,43 @@ import './Navbar.css';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const isMobile = window.innerWidth <= 600; // Definindo um valor de largura para determinar dispositivos móveis
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
-  const closeMenu = () => {
-    setShowMenu(false);
-  };
-
   return (
-    <div className="n-wrapper">
-      <div className="n-left">
-        <div className="name">
-          <span className="first-name">Marcelo</span>
-          <span className="last-name">Amaral</span>
+    <>
+      <div className="n-wrapper">
+        <div className="n-left">
+          <div className="name">
+            <span className="first-name">Marcelo</span>
+            <span className="last-name">Amaral</span>
+          </div>
         </div>
-      </div>
-      <div className={`n-right ${isMobile ? (showMenu ? 'active' : '') : ''}`}>
-        <div className="n-list">
-          {isMobile ? (
-            <span id="burger" className="material-symbols-outlined" onClick={toggleMenu}>
-              menu
-            </span>
-          ) : (
+        <div className={`n-right ${showMenu ? 'active' : ''}`}>
+          <div className="n-list">
             <ul style={{ listStyleType: 'none' }}>
               <li><a href="#about">About</a></li>
               <li><a href="#experience">Experience</a></li>
               <li><a href="#projects">Projects</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
-          )}
-          {isMobile && showMenu && (
-            <div id="itens" className="menu-items">
-              <span className="material-symbols-outlined close-btn" onClick={closeMenu}>
-                close
-              </span>
-
-              <ul style={{ listStyleType: 'none' }}>
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-              </ul>
-            </div>
-          )}
+          </div>
         </div>
       </div>
-    </div>
+      <span id="mobile-menu-icon" class="material-symbols-outlined">
+        menu
+        </span>
+        <div className="mobile-menu">
+          <ul style={{ listStyleType: 'none' }}>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+    </>
   );
 };
 
